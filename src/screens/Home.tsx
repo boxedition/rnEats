75a7908c43b302518f2f3798c637ef4903c63eba
BoxEdition <2111221@my.ipleiria.pt>
2022-01-05 23:32:04 +0000

    Fetch Api

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, View } from 'react-native';
 import HeaderTabs from '../components/HeaderTabs';
@@ -10,21 +10,25 @@ const YELP_API_KEY = "M4sMoPoaR403riZbOVt-xaE0BAifZFMZky_RiFyMS7gpdflgc530-Qk1DF
 
 export default function Home() {
     const [restaurantData, setrestaurantData] = useState(localRestaurants);
-    
+
     const getRestaurantsFromYelp = () => {
         const yelpUrl = "https://api.yelp.com/v3/businesses/search?terms=restaurants&location=Lisboa";
 
         const apiOptions = {
             headers: {
-                Authorzation: `Bearer ${YELP_API_KEY}`,
+                Authorization: `Bearer ${YELP_API_KEY}`,
             },
         };
 
         return fetch(yelpUrl, apiOptions)
             .then((res) => res.json())
+            .then(res => console.log("Result: ",res))
             .then(json => setrestaurantData(json.businesses));
     };
 
+/*     useEffect(() => {
+        getRestaurantsFromYelp();        
+    }, []) */
 
     return (
         <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
